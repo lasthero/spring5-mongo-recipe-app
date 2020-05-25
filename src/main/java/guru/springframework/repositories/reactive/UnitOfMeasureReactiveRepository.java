7faa9ff0f7e727273de59c8d14f52aa0bf256048
chihho-andy-chou <chihho.chou@gmail.com>
2020-05-25 17:00:39 -0400

    Add mongodb reactive

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+
+
+
+}
